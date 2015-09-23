49404b0aa7c54a15ddc6f8fbff280df747ebe258
Pugnator <ookami@mail.ru>
2015-09-24 00:20:20 +0300

    temporaly disabled everething on Linux build

--- firmware/src/linux/main.cc
+++ firmware/src/linux/main.cc
@@ -1,11 +1,9 @@
 #include <global.hpp>
 
-using namespace uart;
-nmeactx nmea;		
-
+//using namespace uart;
 
 int main ( void )
 {
-	Uart dbgout ( 1, 115200, true );
+	//Uart dbgout ( 1, 115200, true );
 	return 0;	
 }

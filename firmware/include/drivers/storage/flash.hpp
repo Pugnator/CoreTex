459b8e0d7ab11a36f7ecc77a631ae757a8caf08b
Pugnator <ookami@mail.ru>
2016-11-07 13:20:51 +0300

    Implementing eeprom via flash

--- firmware/include/drivers/storage/flash.hpp
+++ firmware/include/drivers/storage/flash.hpp
@@ -7,6 +7,10 @@ namespace NAND
 {
 class Flash : public Driver
 {
-
+public:
+	void mass_erase(void);
+	void erase_page(word page);
+	void write_page(word page, uint8_t *buffer);
+	void write_buffer(word page, uint8_t *buffer, word size, word offset);
 };
 }
--- firmware/src/drivers/storage/flash.cc
+++ firmware/src/drivers/storage/flash.cc
@@ -3,8 +3,9 @@
 #define FLASH_KEY1      ((uint32_t)0x45670123)
 #define FLASH_KEY2      ((uint32_t)0xCDEF89AB)
 
+using namespace NAND;
 
-void mass_erase()
+void Flash::mass_erase()
 {
 	FLASH->KEYR = FLASH_KEY1;
 	FLASH->KEYR = FLASH_KEY2;
@@ -14,5 +15,4 @@ void mass_erase()
 	FLASH->CR |= FLASH_CR_STRT; //Start Page Erase
 	while((FLASH->SR & FLASH_SR_BSY));
 	FLASH->CR &= ~FLASH_CR_MER; //Mass Erase Clear
-
 }

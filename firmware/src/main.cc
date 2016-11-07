dfd1fd4501ca6756ceef3f84d52ad45faec06a1e
Pugnator <ookami@mail.ru>
2016-11-07 10:54:28 +0300

    Cleared up main.cc

--- firmware/src/main.cc
+++ firmware/src/main.cc
@@ -17,47 +17,8 @@
 #include <global.hpp>
 #include <common.hpp>
 #include <log.hpp>
-#include <core/spi.hpp>
-#include <drivers/console.hpp>
-#include <drivers/ov528.hpp>
-#include <drivers/storage/fatdisk.hpp>
-#include <drivers/storage/ff.hpp>
 
 int main(void)
 {
-	Uart u(1, CONSOLE_SPEED);
-	Console out(&u);
-	out.cls();
-	out.print("!!!");
-	RTTPRINT("\r\nCore started\r\n");
-	RTTPRINT("FatFs test started\r\n");
-	DISK::FATdisk a(1);
-	DISK::FRESULT r;
-	DISK::FATFS fs;
-	r = a.mount(&fs, "0:", 1);
-	SEGGER_RTT_printf(0,"f_mount %s\r\n", a.result_to_str(r));
-	DISK::FIL test;
-	r = a.open(&test, "test.txt", FA_OPEN_ALWAYS | FA_WRITE |FA_READ);
-	SEGGER_RTT_printf(0,"open %s\r\n", a.result_to_str(r));
-	UINT bw = 0;
-	const char* text= "Hellow world!";
-	r = a.f_write(&test, text, strlen(text), &bw);
-	SEGGER_RTT_printf(0,"f_write %s\r\n", a.result_to_str(r));
-	r = a.close(&test);
-	SEGGER_RTT_printf(0,"close %s\r\n", a.result_to_str(r));
-	//LOGPRINT("Status: 0x%x\r\n", a.get_status());
-	//r = f_mkfs("0:", 0, 0);
-	//LOGPRINT("f_mount? %s\r\n", a.FRESULT2str(r));
-	//FIL test;
-	//r = f_open(&test, "test.txt", FA_OPEN_ALWAYS);
-	//LOGPRINT("f_open? %s\r\n", get_fresult(r));
-	//f_close(&test);
-	//LOGPRINT("f_close? %s\r\n", get_fresult(r));
 	MAIN_END;
-	//dbgout.xprintf("System started\n");
-	//GPS::Gps g(2, 115200);
-	//MODEM::Modem mdm(3, 19200, &dbgout);
-	//dbgout.xprintf("SIM900 %s\n", mdm.ok ? "ON" : "OFF");
-	//dbgout.xprintf("SIM900 setup: %s\n", mdm.setup() ? "OK" : "FAIL");
-	//dbgout.xprintf("SMS number: %lu\n", mdm.get_sms_amount());
 }

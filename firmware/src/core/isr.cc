756ec2bc5c2bc9ec191f0561d84baf50c9a78e97
Pugnator <ookami@mail.ru>
2017-04-16 21:32:49 +0300

    Added all remaining dummy IRQ handlers

--- firmware/src/core/isr.cc
+++ firmware/src/core/isr.cc
@@ -133,6 +133,46 @@ extern "C"
 	}
 
 	void
+	RTC_IRQHandler (void)
+	{
+	}
+	;
+	void
+	FLASH_IRQHandler (void)
+	{
+	}
+	;
+	void
+	RCC_IRQHandler (void)
+	{
+	}
+	;
+	void
+	EXTI0_IRQHandler (void)
+	{
+	}
+	;
+	void
+	EXTI1_IRQHandler (void)
+	{
+	}
+	;
+	void
+	EXTI2_IRQHandler (void)
+	{
+	}
+	;
+	void
+	EXTI3_IRQHandler (void)
+	{
+	}
+	;
+	void
+	EXTI4_IRQHandler (void)
+	{
+	}
+	;
+	void
 	DMA1_Channel1_IRQHandler (void)
 	{
 		if (DMA1->ISR & DMA_ISR_TCIF4)
@@ -140,6 +180,102 @@ extern "C"
 			DMA1->IFCR |= DMA_ISR_TCIF4;
 		}
 	}
+	;
+	void
+	DMA1_Channel2_IRQHandler (void)
+	{
+	}
+	;
+	void
+	DMA1_Channel3_IRQHandler (void)
+	{
+	}
+	;
+	void
+	DMA1_Channel4_IRQHandler (void)
+	{
+	}
+	;
+	void
+	DMA1_Channel5_IRQHandler (void)
+	{
+	}
+	;
+	void
+	DMA1_Channel6_IRQHandler (void)
+	{
+	}
+	;
+	void
+	DMA1_Channel7_IRQHandler (void)
+	{
+	}
+	;
+	void
+	ADC1_2_IRQHandler (void)
+	{
+	}
+	;
+	void
+	USB_HP_CAN1_TX_IRQHandler (void)
+	{
+	}
+	;
+	void
+	USB_LP_CAN1_RX0_IRQHandler (void)
+	{
+	}
+	;
+	void
+	CAN1_RX1_IRQHandler (void)
+	{
+	}
+	;
+	void
+	CAN1_SCE_IRQHandler (void)
+	{
+	}
+	;
+	void
+	EXTI9_5_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM1_BRK_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM1_UP_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM1_TRG_COM_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM1_CC_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM2_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM3_IRQHandler (void)
+	{
+	}
+	;
+	void
+	TIM4_IRQHandler (void)
+	{
+	}
+	;
 
 //TO USE: addr2line -e ./bin/program.elf -a 0x8002327 [GDB: p/x pc when it hit for(;;)]
 	USED void
@@ -217,13 +353,6 @@ extern "C"
 			delayus_asm(300000L);
 			BLINK;
 		}
-	}
-
-	/* DTR */
-	void
-	EXTI0_IRQHandler (void)
-	{
-		//EXTI->PR = EXTI_PR_PR0;
 	}
 
 	void
--- firmware/src/main.cc
+++ firmware/src/main.cc
@@ -65,8 +65,6 @@ void gsm()
 int main (void)
 {
 	SEGGER_RTT_WriteString(0, "Started\r\n");
-	Uart u(1, 9600);
-	Uart u1(1, 9600);
-	u.write(0xaa);
+
 	MAIN_END;
 }

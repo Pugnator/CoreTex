f4295583a994ac13b02cb17de875d6d24405e643
Pugnator <ookami@mail.ru>
2020-01-05 09:21:20 +0300

    Fixing #26

--- HAL/include/core/gpio.hpp
+++ HAL/include/core/gpio.hpp
@@ -5,116 +5,154 @@
 namespace IO
 {
 
-  /* PIN */
+/* PIN */
 
-  typedef enum
+typedef enum
+{
+  PORTA = 0,
+  PORTB,
+  PORTC,
+  PORTD,
+  PORTE
+} IOPORT;
+
+typedef enum
+{
+  P0 = 0,
+  P1,
+  P2,
+  P3,
+  P4,
+  P5,
+  P6,
+  P7,
+  P8,
+  P9,
+  P10,
+  P11,
+  P12,
+  P13,
+  P14,
+  P15
+} IOPIN;
+
+typedef enum
+{
+  IOSPEED_10MHz = 1,
+  IOSPEED_20MHz,
+  IOSPEED_50MHz
+} IOSPEED;
+
+typedef enum
+{
+  IN_PD = 0,
+  IN_PU,
+  IN_FLT,
+  IN_ANALOG,
+  OUT_PP,
+  OUT_OD,
+  OUT_ALT_PP,
+  OUT_ALT_OD
+} IOMODE;
+
+typedef enum
+{
+  RESET = 0,
+  SET,
+  FLOATING
+} PINSTATE;
+
+typedef struct
+{
+  IOPORT port;
+  IOPIN index;
+  IOSPEED speed;
+  IOMODE mode;
+} PINCFG;
+
+class GPIO_pin : public IODriver
+{
+public:
+  GPIO_pin(PINCFG conf, GPIO_pin *isrptr = nullptr);
+  ~GPIO_pin();
+
+public:
+  void toggle();
+  void low();
+  void hi();
+  /* PWM */
+  void pwm(bool enable);
+  void pwm_invert(bool invert);
+  void pwm_duty(uint16_t duty);
+  /* ADC */
+  void adc(bool enable);
+  uint16_t adc_sample();
+  double adc_voltage();
+
+  GPIO_pin *next;
+  void signup();
+  void signout();
+
+  PINSTATE get_state();
+  virtual void isr(uint32_t address);
+
+public:
+  PINCFG get_config();
+
+protected:
+  bool pwm_enabled;
+  bool adc_enabled;
+  ADC_TypeDef *Reg;
+  PINCFG config;
+  GPIO_TypeDef *pbase;
+  GPIO_pin* extirq;
+};
+
+/* PORT */
+
+class GPIO : public IODriver
+{
+public:
+  GPIO(IOPORT _port);
+  ~GPIO();
+
+public:
+  void start();
+  void stop();
+
+private:
+  IOPORT port;
+};
+
+/* PORT iterator */
+
+template <typename T>
+class gpio_iterator : public std::iterator<std::forward_iterator_tag, T>
+{
+  typedef gpio_iterator<T> iterator;
+  GPIO_pin pin;
+
+public:
+  gpio_iterator() : pin(nullptr)
   {
-    PORTA = 0, PORTB, PORTC, PORTD, PORTE
-  } IOPORT;
-
-  typedef enum
+  }
+  ~gpio_iterator()
   {
-    P0 = 0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15
-  } IOPIN;
+  }
+};
 
-  typedef enum
-  {
-    IOSPEED_10MHz = 1, IOSPEED_20MHz, IOSPEED_50MHz
-  } IOSPEED;
-
-  typedef enum
-  {
-    IN_PD = 0, IN_PU, IN_FLT, IN_ANALOG, OUT_PP, OUT_OD, OUT_ALT_PP, OUT_ALT_OD
-  } IOMODE;
-
-  typedef enum
-  {
-    RESET = 0, SET, FLOATING
-  } PINSTATE;
-
-  typedef struct
-  {
-    IOPORT port;
-    IOPIN index;
-    IOSPEED speed;
-    IOMODE mode;
-  } PINCFG;
-
-  class GPIO_pin : public IODriver
-  {
-  public:
-    GPIO_pin (PINCFG conf);
-    ~GPIO_pin ();
-
-  public:
-    void toggle ();
-    void low ();
-    void hi ();
-    /* PWM */
-    void pwm(bool enable);
-    void pwm_invert(bool invert);
-    void pwm_duty(uint16_t duty);
-    /* ADC */
-    void adc(bool enable);
-    uint16_t adc_sample();
-    double adc_voltage();
-
-    PINSTATE get_state ();
-    virtual void isr(uint32_t address);
-  public:
-    PINCFG get_config ();
-  protected:
-    bool pwm_enabled;
-    bool adc_enabled;
-    EXTI_TypeDef* Reg;
-    PINCFG config;
-    GPIO_TypeDef* pbase;
-  };
-
-  /* PORT */
-
-  class GPIO : public IODriver
-  {
-  public:
-    GPIO (IOPORT _port);
-    ~GPIO ();
-
-  public:
-    void start ();
-    void stop ();
-  private:
-    IOPORT port;
-  };
-
-  /* PORT iterator */
-
-  template<typename T>
-    class gpio_iterator : public std::iterator<std::forward_iterator_tag, T>
-    {
-      typedef gpio_iterator<T> iterator;
-      GPIO_pin pin;
-    public:
-      gpio_iterator () :
-          pin (nullptr)
-      {
-      }
-      ~gpio_iterator ()
-      {
-      }
-    };
-
-  template<typename T>
-    gpio_iterator<T>
-    begin (T *val)
-    {
-      return gpio_iterator<T> (val);
-    }
-
-  template<typename T, typename Tsize>
-    gpio_iterator<T>
-    end (T *val, Tsize size)
-    {
-      return gpio_iterator<T> (val) + size;
-    }
-
+template <typename T>
+gpio_iterator<T>
+begin(T *val)
+{
+  return gpio_iterator<T>(val);
 }
+
+template <typename T, typename Tsize>
+gpio_iterator<T>
+end(T *val, Tsize size)
+{
+  return gpio_iterator<T>(val) + size;
+}
+
+} // namespace IO
--- HAL/src/core/gpio.cc
+++ HAL/src/core/gpio.cc
@@ -19,232 +19,278 @@
 #include <core/gpio.hpp>
 #include <common.hpp>
 #include <log.hpp>
+#include <core/stm32f10x.hpp>
 #include <core/isr_helper.hpp>
+
+#define GPIO_DEBUG
+#ifdef GPIO_DEBUG
+#define DEBUG_LOG PrintF
+#else
+#define DEBUG_LOG(...)
+#endif
 
 namespace IO
 {
 
- GPIO_pin::GPIO_pin(PINCFG conf)
- {
-	config = conf;
-	Reg = nullptr;
-	pwm_enabled = false;
-	adc_enabled = false;
-	//Let headers to configure it according to MCU type by itself
-	switch (conf.port)
-	{
-	 case PORTA:
-		pbase = GPIOA;
-		break;
-	 case PORTB:
-		pbase = GPIOB;
-		break;
-	 case PORTC:
-		pbase = GPIOC;
-		break;
-	 case PORTD:
-		pbase = GPIOD;
-		break;
-	 case PORTE:
-		pbase = GPIOE;
-		break;
-	 default:
-		Print("Wrong port provided\n");
-		__assert(!conf.port, __FILE__, __LINE__);
-		break;
-	}
+GPIO_pin::GPIO_pin(PINCFG conf, GPIO_pin *isrptr)
+{
+  extirq = isrptr;
+  signup();
+  config = conf;
+  Reg = nullptr;
+  pwm_enabled = false;
+  adc_enabled = false;
+  //Let headers to configure it according to MCU type by itself
+  switch (conf.port)
+  {
+  case PORTA:
+    pbase = GPIOA;
+    break;
+  case PORTB:
+    pbase = GPIOB;
+    break;
+  case PORTC:
+    pbase = GPIOC;
+    break;
+  case PORTD:
+    pbase = GPIOD;
+    break;
+  case PORTE:
+    pbase = GPIOE;
+    break;
+  default:
+    Print("Wrong port provided\n");
+    __assert(!conf.port, __FILE__, __LINE__);
+    break;
+  }
 
-	((uint32_t*) pbase)[config.index >> 3u] &= ~(0x0Fu << ((config.index % 8u) << 2u));
-	switch (config.mode)
-	{
-	 case IN_PD:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x08u | 0) << ((config.index % 8u) << 2u);
-		pbase->BRR = (1u << config.index);
-		break;
-	 case IN_PU:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x08u | 0) << ((config.index % 8u) << 2u);
-		pbase->BSRR = (1u << config.index);
-		break;
-	 case IN_FLT:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x04u | config.speed) << ((config.index % 8u) << 2u);
-		break;
-	 case IN_ANALOG:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0 | config.speed) << ((config.index % 8u) << 2u);
-		break;
-	 case OUT_PP:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x00u | config.speed) << ((config.index % 8u) << 2u);
-		break;
-	 case OUT_OD:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x04u | config.speed) << ((config.index % 8u) << 2u);
-		break;
-	 case OUT_ALT_PP:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x08u | config.speed) << ((config.index % 8u) << 2u);
-		break;
-	 case OUT_ALT_OD:
-		((uint32_t*) pbase)[config.index >> 3u] |= (0x0Cu | config.speed) << ((config.index % 8u) << 2u);
-		break;
-	 default:
-		Print("Wrong pin mode provided\n");
-		__assert(!conf.port, __FILE__, __LINE__);
-		break;
-	}
+  ((uint32_t *)pbase)[config.index >> 3u] &= ~(0x0Fu << ((config.index % 8u) << 2u));
+  switch (config.mode)
+  {
+  case IN_PD:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x08u | 0) << ((config.index % 8u) << 2u);
+    pbase->BRR = (1u << config.index);
+    break;
+  case IN_PU:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x08u | 0) << ((config.index % 8u) << 2u);
+    pbase->BSRR = (1u << config.index);
+    break;
+  case IN_FLT:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x04u | config.speed) << ((config.index % 8u) << 2u);
+    break;
+  case IN_ANALOG:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0 | config.speed) << ((config.index % 8u) << 2u);
+    break;
+  case OUT_PP:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x00u | config.speed) << ((config.index % 8u) << 2u);
+    break;
+  case OUT_OD:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x04u | config.speed) << ((config.index % 8u) << 2u);
+    break;
+  case OUT_ALT_PP:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x08u | config.speed) << ((config.index % 8u) << 2u);
+    break;
+  case OUT_ALT_OD:
+    ((uint32_t *)pbase)[config.index >> 3u] |= (0x0Cu | config.speed) << ((config.index % 8u) << 2u);
+    break;
+  default:
+    Print("Wrong pin mode provided\n");
+    __assert(!conf.port, __FILE__, __LINE__);
+    break;
+  }
 
-	HARDWARE_TABLE[EXTI0_HANDLER] = (uint32_t) this;
-	//int irqnum = USART1_IRQn - 1;
-	//NVIC_EnableIRQ((IRQn_Type) irqnum);
-	//NVIC_SetPriority((IRQn_Type) irqnum, 3);
- }
-
- GPIO_pin::~GPIO_pin()
- {
- }
-
- void GPIO_pin::isr(uint32_t address)
- {
-
- }
-
- void GPIO_pin::low()
- {
-	pbase->BRR = (1u << config.index);
- }
-
- void GPIO_pin::hi()
- {
-	pbase->BSRR = (1u << config.index);
- }
-
- void GPIO_pin::toggle()
- {
-	pbase->BSRR =
-	  (pbase->ODR & (1u << config.index)) ? ((1u << config.index) << 16u) : (1u << config.index);
- }
-
- void GPIO_pin::pwm(bool enable)
- {
-	pwm_enabled = enable;
-	if (GPIOA == pbase)
-	{
-	 switch (config.index)
-	 {
-		case P0:
-		 RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-		 TIM2->CCER |= TIM_CCER_CC1E; //enable pin
-		 TIM2->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
-		 TIM2->CCMR1 &= ~TIM_CCMR1_OC1M_0;
-		 break;
-		case P1:
-		 RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-		 TIM2->CCER |= TIM_CCER_CC2E; //enable pin
-		 TIM2->CCMR1 |= TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2;
-		 TIM2->CCMR1 &= ~TIM_CCMR1_OC2M_0;
-		 break;
-		case P2:
-		 RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-		 TIM2->CCER |= TIM_CCER_CC3E; //enable pin
-		 TIM2->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2;
-		 TIM2->CCMR2 &= ~TIM_CCMR2_OC3M_0;
-		 break;
-		case P3:
-		 RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-		 TIM2->CCER |= TIM_CCER_CC4E; //enable pin
-		 TIM2->CCMR2 |= TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_2;
-		 TIM2->CCMR2 &= ~TIM_CCMR2_OC4M_0;
-		 break;
-	 }
-
-	 if (enable)
-	 {
-		TIM2->CR1 |= TIM_CR1_CEN;
-	 }
-	 else
-	 {
-		TIM2->CR1 &= ~TIM_CR1_CEN;
-	 }
-	}
-	else if (GPIOB == pbase)
-	{
-
-	}
- }
-
- void GPIO_pin::pwm_invert(bool invert)
- {
-	if (!pwm_enabled)
-	{
-	 pwm(true);
-	}
- }
-
- void GPIO_pin::pwm_duty(uint16_t duty)
- {
-	if (!pwm_enabled)
-	{
-	 pwm(true);
-	}
-	if (GPIOA == pbase)
-	{
-	 switch (config.index)
-	 {
-		case P0:
-		case P1:
-		case P2:
-		case P3:
-		 TIM2->CCR2 = (0xFFFF / 100) * duty;
-		 break;
-	 }
-	}
- }
-
- void GPIO_pin::adc(bool enable)
- {
-	adc_enabled = enable;
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
-	RCC->CFGR |= RCC_CFGR_ADCPRE;
-	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV8;
-
-	ADC1->CR2 |= ADC_CR2_CAL;
-	while (!(ADC1->CR2 & ADC_CR2_CAL))
-	 ;
-
-	ADC1->SQR1 = ADC_SQR3_SQ1_0;
-	ADC1->SQR2 = 0;
-	ADC1->SQR3 = 1;//ADC_SQR3_SQ1_0 | ADC_SQR3_SQ1_3;
-
-	ADC1->CR2 |= (ADC_CR2_EXTSEL_0 | ADC_CR2_EXTSEL_1 | ADC_CR2_EXTSEL_2 | ADC_CR2_EXTTRIG);
-	ADC1->CR2 |= ADC_CR2_ADON;
- }
-
- uint16_t GPIO_pin::adc_sample()
- {
-	if (!adc_enabled)
-	{
-	 adc(true);
-	}
-	ADC1->CR2 |= ADC_CR2_SWSTART;
-	while (!(ADC1->SR & ADC_SR_EOC))
-	 ;
-	uint16_t result = ADC1->DR & 0xFFFF;
-	ADC1->SR = 0;
-	return result;
- }
-
- double GPIO_pin::adc_voltage()
- {
-	return adc_sample() / 4096 * 3;
- }
-
- PINSTATE GPIO_pin::get_state()
- {
-	return pbase->IDR & (1u << config.index) ? SET : RESET;
- }
-
- GPIO::GPIO(IOPORT _port)
- {
-	port = _port;
- }
-
- GPIO::~GPIO()
- {
- }
+  //HARDWARE_TABLE[EXTI0_HANDLER] = (uint32_t)this;
+  //int irqnum = USART1_IRQn - 1;
+  //NVIC_EnableIRQ((IRQn_Type) irqnum);
+  //NVIC_SetPriority((IRQn_Type) irqnum, 3);
 }
+
+GPIO_pin::~GPIO_pin()
+{
+  signout();
+}
+
+void GPIO_pin::isr(uint32_t address)
+{
+  if (address)
+  {
+    DEBUG_LOG("GPIO_pin IRQ registration: 0x%X\r\n", address);
+    next = reinterpret_cast<GPIO_pin *>(address);
+    return;
+  }
+
+  if (Reg->SR & ADC_SR_EOC)
+  {
+    uint16_t result = ADC1->DR & 0xFFFF;
+    ADC1->SR = 0;
+  }
+}
+
+void GPIO_pin::signup()
+{
+  Reg = (ADC_TypeDef *)ADC1_BASE;
+
+  GPIO_pin *i = (GPIO_pin *)HARDWARE_TABLE[GPIO_HANDLER];
+  if (i)
+  {
+    DEBUG_LOG("Another instance of GPIO is registered 0x%X, adding myself 0x%X\r\n", (uint32_t)i, (uint32_t)this);
+    i->isr(this);
+  }
+  else
+  {
+    DEBUG_LOG("First GPIO handler registration 0x%X\r\n", reinterpret_cast<uint32_t>(this));
+    HARDWARE_TABLE[GPIO_HANDLER] = extirq ? reinterpret_cast<uint32_t>(extirq) : reinterpret_cast<uint32_t>(this);
+  }
+}
+
+void GPIO_pin::signout()
+{
+  HARDWARE_TABLE[GPIO_HANDLER] = next ? (uint32_t)next : 0;
+}
+
+void GPIO_pin::low()
+{
+  pbase->BRR = (1u << config.index);
+}
+
+void GPIO_pin::hi()
+{
+  pbase->BSRR = (1u << config.index);
+}
+
+void GPIO_pin::toggle()
+{
+  pbase->BSRR =
+      (pbase->ODR & (1u << config.index)) ? ((1u << config.index) << 16u) : (1u << config.index);
+}
+
+void GPIO_pin::pwm(bool enable)
+{
+  pwm_enabled = enable;
+  if (GPIOA == pbase)
+  {
+    switch (config.index)
+    {
+    case P0:
+      RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+      TIM2->CCER |= TIM_CCER_CC1E; //enable pin
+      TIM2->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+      TIM2->CCMR1 &= ~TIM_CCMR1_OC1M_0;
+      break;
+    case P1:
+      RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+      TIM2->CCER |= TIM_CCER_CC2E; //enable pin
+      TIM2->CCMR1 |= TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2;
+      TIM2->CCMR1 &= ~TIM_CCMR1_OC2M_0;
+      break;
+    case P2:
+      RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+      TIM2->CCER |= TIM_CCER_CC3E; //enable pin
+      TIM2->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2;
+      TIM2->CCMR2 &= ~TIM_CCMR2_OC3M_0;
+      break;
+    case P3:
+      RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+      TIM2->CCER |= TIM_CCER_CC4E; //enable pin
+      TIM2->CCMR2 |= TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_2;
+      TIM2->CCMR2 &= ~TIM_CCMR2_OC4M_0;
+      break;
+    }
+
+    if (enable)
+    {
+      TIM2->CR1 |= TIM_CR1_CEN;
+    }
+    else
+    {
+      TIM2->CR1 &= ~TIM_CR1_CEN;
+    }
+  }
+  else if (GPIOB == pbase)
+  {
+  }
+}
+
+void GPIO_pin::pwm_invert(bool invert)
+{
+  if (!pwm_enabled)
+  {
+    pwm(true);
+  }
+}
+
+void GPIO_pin::pwm_duty(uint16_t duty)
+{
+  if (!pwm_enabled)
+  {
+    pwm(true);
+  }
+  if (GPIOA == pbase)
+  {
+    switch (config.index)
+    {
+    case P0:
+    case P1:
+    case P2:
+    case P3:
+      TIM2->CCR2 = (0xFFFF / 100) * duty;
+      break;
+    }
+  }
+}
+
+void GPIO_pin::adc(bool enable)
+{
+  adc_enabled = enable;
+  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+  RCC->CFGR |= RCC_CFGR_ADCPRE;
+  RCC->CFGR |= RCC_CFGR_ADCPRE_DIV8;
+
+  ADC1->CR2 |= ADC_CR2_CAL;
+  while (!(ADC1->CR2 & ADC_CR2_CAL))
+    ;
+
+  ADC1->SQR1 = ADC_SQR3_SQ1_0;
+  ADC1->SQR2 = 0;
+  ADC1->SQR3 = 1; //ADC_SQR3_SQ1_0 | ADC_SQR3_SQ1_3;
+
+  ADC1->CR2 |= (ADC_CR2_EXTSEL_0 | ADC_CR2_EXTSEL_1 | ADC_CR2_EXTSEL_2 | ADC_CR2_EXTTRIG);
+  ADC1->CR1 = ADC_CR1_EOCIE; // Enable interrupt form End Of Conversion
+  NVIC_EnableIRQ(ADC1_2_IRQn);
+  ADC1->CR2 |= ADC_CR2_ADON | ADC_CR2_CONT | ADC_CR2_SWSTART;
+}
+
+uint16_t GPIO_pin::adc_sample()
+{
+  if (!adc_enabled)
+  {
+    adc(true);
+  }
+  ADC1->CR2 |= ADC_CR2_SWSTART;
+  while (!(ADC1->SR & ADC_SR_EOC))
+    ;
+  uint16_t result = ADC1->DR & 0xFFFF;
+  ADC1->SR = 0;
+  return 0;
+  return result;
+}
+
+double GPIO_pin::adc_voltage()
+{
+  return adc_sample() / 4096 * 3;
+}
+
+PINSTATE GPIO_pin::get_state()
+{
+  return pbase->IDR & (1u << config.index) ? SET : RESET;
+}
+
+GPIO::GPIO(IOPORT _port)
+{
+  port = _port;
+}
+
+GPIO::~GPIO()
+{
+}
+} // namespace IO
